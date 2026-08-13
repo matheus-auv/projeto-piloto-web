d035da81d59557c3c3874a26faf61b840ad315b4
matheus-auv <matheus@auvin-technologies.com.br>
2026-08-13 17:51:45 -0300

    wip: começando o componente de card de resumo

--- src/pages/home/components/resumo-card/resumo-card.tsx
+++ src/pages/home/components/resumo-card/resumo-card.tsx
@@ -1,3 +1,16 @@
-export const ResumoCard = () => {
-  return <div></div>;
+type ResumoCardProps = {
+  titulo: string;
+  conteudo: string;
+};
+
+export const ResumoCard = ({ titulo, conteudo }: ResumoCardProps) => {
+  return (
+    <div
+      className="rounded-lg border border-slate-400 bg-white p-4
+      shadow-sm transition hover:shadow-lg mr-4 mt-4"
+    >
+      <h1 className="text-lg font-bold flex items-center">{titulo}</h1>
+      <p>{conteudo}</p>
+    </div>
+  );
 };
--- src/pages/home/home.layout.tsx
+++ src/pages/home/home.layout.tsx
@@ -1,7 +1,42 @@
+import { ResumoCard } from './components/resumo-card';
+
+const MOCK_RESUMOS = [
+  {
+    titulo: 'sei la',
+    conteudo: 'sei la sei la 1',
+  },
+  {
+    titulo: 'sei la',
+    conteudo: 'sei la sei la 2',
+  },
+  {
+    titulo: 'sei la',
+    conteudo: 'sei la sei la 3',
+  },
+  {
+    titulo: 'sei la',
+    conteudo: 'sei la sei la 4',
+  },
+];
+
 export const HomeLayout = () => {
   return (
-    <div>
-      <h1>Fodase</h1>
-    </div>
+    <main className="min-h-screen p-8 bg-sky-50 text-slate-900">
+      <header className="mx-auto w-full max-w-300">
+        <h1 className="text-4xl font-bold">Projeto Piloto</h1>
+      </header>
+
+      <section className="mx-auto mt-12 w-full max-w-300">
+        <div className="mb-4">
+          <button>Adicionar resumo</button>
+        </div>
+
+        <div className="grid grid-cols-3">
+          {MOCK_RESUMOS.map(resumo => (
+            <ResumoCard titulo={resumo.titulo} conteudo={resumo.conteudo} />
+          ))}
+        </div>
+      </section>
+    </main>
   );
 };
